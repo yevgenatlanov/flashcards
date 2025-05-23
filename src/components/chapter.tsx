e7c2c8e117cc9e02f7dabd3fa1411a682aed5c1a
Yevgen Atlanov <yevgen.atlanov@gmail.com>
2025-05-23 15:58:55 +0200

    card upgrade

--- src/components/chapter.tsx
+++ src/components/chapter.tsx
@@ -1,6 +1,5 @@
 import { ChapterWithTasks } from "@/types";
 import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 
 // Chapter Selection Componentex
